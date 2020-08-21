844165e3f0d1e6b4f48ab7e76aa469fbe462200c
Xavier Jackson <xavierejackson20@gmail.com>
2020-08-20 21:05:22 -0400

    updated server file

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-// Dependencies
+//Dependencies
 const express = require("express");
 const logger = require("morgan");
-const mongojs = require("mongojs");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
+//PORT
+const PORT = process.env.PORT || 3000;
+
+//application middleware
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// connecting to the database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
+//Routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Server
+app.listen(3000, () => {
+  console.log(`App running on port http://localhost:${PORT}`);
+});
